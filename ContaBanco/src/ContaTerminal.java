ad94a8f507a7ee6d5d508bad9f46f16ada313b0f
Bruno Martucci <brunomartucci77@gmail.com>
2024-05-02 17:12:33 -0300

    Desafio finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroConta;
+        String agenciaCliente;
+        String nomeCliente;
+        double saldoContaCliente;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Deseja abrir uma conta nova ? (S/N)");
+        String escolhaAbrirConta = scanner.next();
+
+        if (escolhaAbrirConta.equals("S")) {
+            System.out.print("Digite o numero da conta: ");
+            numeroConta = scanner.nextInt();
+            System.out.print("Digite a agência da conta: ");
+            agenciaCliente = scanner.next();
+            System.out.print("Digite o nome do cliente: ");
+            nomeCliente = scanner.next();
+            System.out.print("Digite o saldo inicial da conta: ");
+            saldoContaCliente = scanner.nextDouble();
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agenciaCliente + ", conta " + numeroConta + " e seu saldo  " + saldoContaCliente  + " já está disponivel para saque.");
+        }else{
+            System.out.println("Operação finalizada !");
+        }
+        scanner.close();
     }
 }
